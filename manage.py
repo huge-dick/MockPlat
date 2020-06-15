84804468c30c6d1fb3dbd0f7806d877e40c75511
wangjie <1126775097@qq.com>
2020-06-15 19:12:29 +0800

    firstcommit

---
+++ manage.py
@@ -0,0 +1,15 @@
+from flask_script import Manager
+from app import app
+
+manager = Manager(app) # 注册一个manager实例，自带了runserver命令
+
+@manager.command
+def hello():
+    """这只是一个测试命令"""
+    print("OK")
+    return "Hello World!"
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8001, threaded=True)
